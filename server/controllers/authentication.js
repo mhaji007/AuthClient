22c8a6402bd2ecd8a87feadc224dbab351d0b478
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-18 18:29:53 -0400

    Search for users

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,16 @@
+const user = require('../models/user');
+
 exports.signup = function(req, res, next) {
-    res.send({success:'true'})
+    const email = req.body.email;
+    const password = req.body.password;
+
+   // See of a user with a given email exists
+    user.findOne({email:email}, function (err, existingUser) {
+        
+    });
+   // If a user with email does exist, retrun an error
+
+   // If a user with email does not exist, create and save user record
+
+   // Respond to request indicating the user was created
 }
