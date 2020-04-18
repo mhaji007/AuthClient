02ff033c99c833432c90c492ae6b8abffa4d149f
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-18 15:58:25 -0400

    Set up route handlers

--- server/index.js
+++ server/index.js
@@ -5,12 +5,13 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
-const app = express()
+const app = express();
+const router = require('./router');
 
 // App setup
 app.use(morgan('combined'));
 app.use(bodyParser.json({type:'*/*'}));
-
+router(app);
 
 // Server Setup
 const port = process.env.PORT||3090;
