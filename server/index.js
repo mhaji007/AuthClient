074f45704fe0d14d90fdb7450556aaa1dbe1cff0
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-18 15:42:35 -0400

    More server setup

--- server/index.js
+++ server/index.js
@@ -1,4 +1,22 @@
 // Main starting point of the application
 
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express()
+
+// App setup
+
+
+
+// Server Setup
+const port = process.env.PORT||3090;
+const server = http.createServer(app);
+server.listen (port);
+console.log('Server listening on:', port);
+
+
 
 
