73a6b801d75e49f73273ccf92fe88dc8452e2163
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-18 22:15:37 -0400

    Create user records

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,16 +1,34 @@
-const user = require('../models/user');
+const User = require('../models/users');
 
-exports.signup = function(req, res, next) {
+exports.signup = function (req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
 
-   // See of a user with a given email exists
-    user.findOne({email:email}, function (err, existingUser) {
-        
+    // See of a user with a given email exists
+    User.findOne({
+        email: email
+    }, function (err, existingUser) {
+        if (err) {
+            return next(err);
+        }
+        // If a user with email does exist, return an error
+        if (existingUser) {
+            return res
+                .status(422)
+                .send({error: 'Email is in use'});
+        }
+
+        // If a user with email does not exist, create and save user record Respond to
+        // request indicating the user was created
+        const user = new User({email: email, password: password});
+
+        user.save(function (err) {
+            if (err) {
+                return next(err);
+            }
+            res.json({success:true});
+        });
+
+
     });
-   // If a user with email does exist, retrun an error
-
-   // If a user with email does not exist, create and save user record
-
-   // Respond to request indicating the user was created
 }
