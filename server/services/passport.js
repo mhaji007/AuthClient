0bd82bd6225631928513f347fb7c1ef5ebc17ea0
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-19 19:13:27 -0400

    Make an authenticated request

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,26 +1,36 @@
 const passport = require('passport');
-const User = require('../models/user');
+const User = require('../models/users');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
-// Setup options for JWT strategy
-const jwtOptions = {};
+// Setup configuration options for JWT strategy
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    secretOrKey: config.secret
+};
 
 //Create JWT strategy
 const jwtLogin = new JwtStrategy (jwtOptions, function(payload, done){
+// Payload is the decoded jwt token
 // See if the user ID in the payload exists in our database
 // If it does, call 'done wiht that other
 //otherwise, call donw without a user object
 
 User.findById(payload.sub, function(err,user){
+    
+    // if search process fails
     if(err) {return done(err,false);}
-
+    // if user found
     if (user) {
         done(null, user);
+    // if user not found
     } else {
         done(null,false);
     }
 });
 
 });
+
+// Tell Passport to use this strategy
+passport.use(jwtLogin);
