c1cc487a421b9999217e91b722231a169f5c5b82
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-19 17:57:25 -0400

    Include Passport strategy

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -4,7 +4,7 @@ const config = require('../config');
 
 function tokenForUser(user) {
     const timestamp = new Date().getTime();
-    return jwt.encode({sub: user.id}, config.secret)
+    return jwt.encode({sub: user.id, iat:timestamp}, config.secret)
 }
 
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -4,5 +4,23 @@ const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
+// Setup options for JWT strategy
+const jwtOptions = {};
 
+//Create JWT strategy
+const jwtLogin = new JwtStrategy (jwtOptions, function(payload, done){
+// See if the user ID in the payload exists in our database
+// If it does, call 'done wiht that other
+//otherwise, call donw without a user object
 
+User.findById(payload.sub, function(err,user){
+    if(err) {return done(err,false);}
+
+    if (user) {
+        done(null, user);
+    } else {
+        done(null,false);
+    }
+});
+
+});
