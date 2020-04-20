42f63ee2919c765f83a51d2a55322df7e8da3928
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-20 12:55:41 -0400

    Create the header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
 export default () => {
     return (
         <div>
+            <Header/>
             Hi, I'm the app
         </div>
     )
