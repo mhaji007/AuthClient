4a0f5c3618d2ced15787dffa8fc5256a1d7a6a0d
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-20 13:09:11 -0400

    Wire up React Router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Header from './Header';
 
-export default () => {
+export default ({children}) => {
     return (
         <div>
             <Header/>
-            Hi, I'm the app
+            {children}
         </div>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 import App from './components/App';
+import Welcome from './components/welcome';
 
 
 ReactDOM.render(
-    <App/>,
+    <BrowserRouter>
+    <App>
+        <Route path="/" exact component={Welcome}></Route>
+    </App>
+    </BrowserRouter>
+    ,
     document.querySelector('#root')
 )
