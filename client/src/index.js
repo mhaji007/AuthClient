71196890e80309254568776156d2531ddaa58282
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-20 14:36:58 -0400

    Wire up Redux Thunk

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route} from 'react-router-dom';
 import {Provider}from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
 import App from './components/App';
 import Welcome from './components/welcome';
 import Signup from './components/auth/Signup';
 
+const store =createStore(
+    reducers,
+    {},
+    applyMiddleware(reduxThunk)
+);
 
 ReactDOM.render(
-    <Provider store={createStore(reducers, {})}>
+    <Provider store={store}>
     <BrowserRouter>
     <App>
         <Route path="/" exact component={Welcome}></Route>
