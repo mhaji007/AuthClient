14e7c0932707fbcdfce68f7df12f95b712470e17
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-20 17:24:24 -0400

    Dispatch actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import {AUTH_USER} from './types';
 
-export const signup = formProps => dispatch =>{
-        axios.post('http://localhost:3090/signup', formProps);
+export const signup = formProps => async dispatch =>{
+         const response =await axios.post('http://localhost:3090/signup', formProps);
+
+         dispatch({type:AUTH_USER, payload: response.data.token});
 };
+
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,8 +1,20 @@
+import {AUTH_USER} from'../actions/types';
+
+
+
 const INITIAL_STATE = {
     authenticated: '',
     errorMessage: ''
 }
 
 export default function (state = INITIAL_STATE, action ) {
- return state;
+ 
+    switch(action.type) {
+        case AUTH_USER:
+            return {...state, authenticated: action.payload}
+
+        default:
+            return state;
+}
+
 }
