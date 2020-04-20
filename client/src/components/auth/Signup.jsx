3a9a5593c28e85b1fe9f47e23512be37507006a8
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-20 13:24:52 -0400

    Scaffold the sign up form

---
+++ client/src/components/auth/Signup.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Signup extends Component {
+    state = {  }
+    render() { 
+        return (
+        <form>
+            <fieldset>
+                <label htmlFor="">Email</label>
+            </fieldset>
+            <fieldset>
+                <label htmlFor="">Email</label>
+            </fieldset>
+        </form>);
+    }
+}
+ 
+export default Signup;
--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,14 @@ import {BrowserRouter, Route} from 'react-router-dom';
 
 import App from './components/App';
 import Welcome from './components/welcome';
+import Signup from './components/auth/Signup';
 
 
 ReactDOM.render(
     <BrowserRouter>
     <App>
         <Route path="/" exact component={Welcome}></Route>
+        <Route path="/signup" exact component={Signup}></Route>
     </App>
     </BrowserRouter>
     ,
