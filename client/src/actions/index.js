ac372e56f8a609a0dd994580bc54f03c7e11426f
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-21 14:17:21 -0400

    Add Signin action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -13,6 +13,18 @@ export const signup = (formProps, callback) => async dispatch =>{
          dispatch({type: AUTH_ERROR, payload: 'Email in use'});
         }
 };
+export const signin = (formProps, callback) => async dispatch =>{
+        
+        try {
+        const response = await axios.post('http://localhost:3090/signin', formProps);
+
+         dispatch({type:AUTH_USER, payload: response.data.token});
+         localStorage.setItem('token', response.data.token);
+         callback();
+        } catch (e) {
+         dispatch({type: AUTH_ERROR, payload: 'Invalid login credentials'});
+        }
+};
 
 export const signout = () => {
    localStorage.removeItem('token'); 
