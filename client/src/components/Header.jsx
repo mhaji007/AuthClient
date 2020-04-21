656049dbc7567ad07d82391fb671580398a80560
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-21 14:27:38 -0400

    Refactor header for authentication based access

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 class  Header extends Component {
+
+    renderLinks(){
+        if(this.props.authenticated) {
+            return (
+                <div>
+                    <Link to="/signout">Sign Out</Link>
+                    <Link to="/feature">Feature</Link>
+                </div>
+            );    
+        } else {
+            return (
+            <div>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/signin">Sign In</Link>
+            </div>
+            );
+        }
+    }
+
     render() { 
         return ( 
             <div>
                 <Link to="/">Redux Auth</Link>
-                <Link to="/signup">Sign Up</Link>
-                <Link to="/signin">Sign In</Link>
-                <Link to="/signout">Sign Out</Link>
-                <Link to="/feature">Feature</Link>
+                {this.renderLinks()}
             </div>
          );
     }
 }
+
+function mapStateToProps(state) {
+    return {authenticated: state.auth.authenticated}
+}
  
-export default  Header;
+export default connect(mapStateToProps)(Header);
